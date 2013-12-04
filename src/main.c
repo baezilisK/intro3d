292c3f0ed57b186070465fe25f4ab4764c58bf68
baezilisK <baezilisk@gmail.com>
2013-12-05 10:46:28 +1100

    new level file

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ static void init (void) {
   glClearColor (0, 0, 0, 0);
   glEnable (GL_DEPTH_TEST);
   kbd_setmap ("assets/kbd/dvp");
-  if (levelio_read ("assets/levels/10")) {
+  if (levelio_read ("assets/levels/11")) {
     fprintf (stderr, "fatal: specified level does not exist\n");
     exit (EXIT_FAILURE);
   }
