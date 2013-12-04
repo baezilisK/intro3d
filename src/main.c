4db903e39798b895145551ee7495f42b7522e661
baezilisK <baezilisk@gmail.com>
2013-12-04 12:17:41 +1100

    larger test map

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ static void init (void) {
   glClearColor (0, 0, 0, 0);
   glEnable (GL_DEPTH_TEST);
   kbd_setmap ("assets/kbd/dvp");
-  if (levelio_read ("assets/levels/9")) {
+  if (levelio_read ("assets/levels/10")) {
     fprintf (stderr, "fatal: specified level does not exist\n");
     exit (EXIT_FAILURE);
   }
